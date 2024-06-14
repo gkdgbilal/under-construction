889ad9658510ca34ea8e0c2dd4055849d768a8f8
Bilal Gokdag <bilal.gkdg@gmail.com>
2024-06-14 11:34:12 +0300

    Fix: Added header section & layout component

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='bg-gray-900 text-white w-full py-6'>
+      <div className='container mx-auto flex justify-center items-center'>
+        <h1 className='text-2xl font-bold text-center w-full'>
+          Alperen Gökdağ Hukuk Bürosu
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
